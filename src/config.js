4a6342239db8624ebf616c98c67d58bcf8099aa6
William Holcombe <sphinxelite@Williams-iMac.local>
2018-11-11 14:31:04 -0500

    push

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 export const API_BASE_URL =
   process.env.REACT_APP_API_BASE_URL ||
-  "https://ancient-springs-33391.herokuapp.com/";
+  "https://ancient-springs-33391.herokuapp.com";
 
 export const DEV_API_BASE_URL =
   process.env.REACT_APP_API_BASE_URL || "http://localhost:8080";
