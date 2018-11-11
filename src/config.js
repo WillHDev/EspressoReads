b4305be0ad1108d6be79dbcbd9c1c318d2c98b82
William Holcombe <sphinxelite@Williams-iMac.local>
2018-11-11 14:19:01 -0500

    variable

--- src/config.js
+++ src/config.js
@@ -1,5 +1,6 @@
 export const API_BASE_URL =
-  process.env.REACT_APP_API_BASE_URL || "http://localhost:8080";
+  process.env.REACT_APP_API_BASE_URL ||
+  "https://ancient-springs-33391.herokuapp.com/";
 
 export const DEV_API_BASE_URL =
   process.env.REACT_APP_API_BASE_URL || "http://localhost:8080";
