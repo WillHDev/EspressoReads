c968250a89a13162308a3d63786c0841aaa667b2
William Holcombe <sphinxelite@Williams-iMac.local>
2018-11-13 12:08:39 -0500

    favicon

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 export const API_BASE_URL =
-  process.env.REACT_APP_API_BASE_URL ||
-  "https://ancient-springs-33391.herokuapp.com";
+  process.env.REACT_APP_API_BASE_URL || "http://localhost:8080";
+//"https://ancient-springs-33391.herokuapp.com";
 
 export const DEV_API_BASE_URL =
   process.env.REACT_APP_API_BASE_URL || "http://localhost:8080";
